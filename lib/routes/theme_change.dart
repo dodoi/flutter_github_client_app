52ad89cff1bbe45b5a288c223bddbfcac4bfa17a
liujia@dabanjia.com <liujia@dabanjia.com>
2020-06-12 12:09:35 +0800

    页面开发

---
+++ lib/routes/theme_change.dart
@@ -0,0 +1,29 @@
+import '../index.dart';
+
+class ThemeChangeRoute extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(GmLocalizations.of(context).theme),
+      ),
+      body: ListView( //显示主题色块
+        children: Global.themes.map<Widget>((e) {
+          return GestureDetector(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 0),
+              child: Container(
+                color: e,
+                height: 80,
+              ),
+            ),
+            onTap: () {
+              //主题更新后，MaterialApp会重新build
+              Provider.of<ThemeModel>(context).theme = e;
+            },
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
